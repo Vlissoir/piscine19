d49348225cad862b6d2bfa3efff526fb5a2d9ab1
Vlissoir <71093239+Vlissoir@users.noreply.github.com>
2020-09-18 16:55:43 +0200

    Add files via upload

---
+++ C02Retry2/ex10/ft_strlcpy.c
@@ -0,0 +1,36 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strlcpy.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: vlissoir <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/09/16 16:31:36 by vlissoir          #+#    #+#             */
+/*   Updated: 2020/09/17 08:08:25 by vlissoir         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+unsigned int	ft_strlcpy(char *dest, char *src, unsigned int size)
+{
+	unsigned int i;
+	unsigned int n;
+
+	i = 0;
+	n = 0;
+	while (src[n] != '\0')
+	{
+		n++;
+	}
+	if (size != 0)
+	{
+		while (src[i] != '\0' && i < (size - 1))
+		{
+			dest[i] = src[i];
+			i++;
+		}
+		dest[i] = '\0';
+	}
+	return (n);
+}
